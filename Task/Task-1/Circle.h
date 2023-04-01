e3f8b5af3f0107be2d043bcf828a13186381e510
darkis <113136403+Bodichane@users.noreply.github.com>
2023-04-01 23:52:54 +0300

    Update Circle.h

--- Task/Task-1/Circle.h
+++ Task/Task-1/Circle.h
@@ -3,15 +3,12 @@
 #include "Form.h"
 #include "Point.h"
 
-class Circle : public Form, Point
+class Circle : public Form
 {
 	public:
 		Circle();
 		Circle(unsigned int radius, const Point& center);
-		string ToString() const override;
-		void FromString(const std::string& str) override;
-		static bool Validate(unsigned int radius);
-		static void ValidateAndThrow(unsigned int radius);
+		std::string ToString() const override;
 
 	unsigned int radius;
 	Point center;
