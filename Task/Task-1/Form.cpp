5841dd99edf7eabb81c3ae0ee64bf28964f8e190
darkis <113136403+Bodichane@users.noreply.github.com>
2023-04-01 23:52:15 +0300

    Update Form.cpp

--- Task/Task-1/Form.cpp
+++ Task/Task-1/Form.cpp
@@ -1,15 +1,3 @@
 #include "Form.h"
 
 Form::~Form() {}
-
-void Form::Draw() const
-{
-	std::cout << ToString() << std::endl;
-}
-
-void Form::Read(std::istream& is)
-{
-	std::string str;
-	std::getline(is, str);
-	FromString(str);
-}
