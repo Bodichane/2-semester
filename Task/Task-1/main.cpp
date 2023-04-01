d71003dd2a2aa2a2152be39cbb1e58cb1e4fec9f
darkis <113136403+Bodichane@users.noreply.github.com>
2023-04-01 23:50:29 +0300

    Update main.cpp

--- Task/Task-1/main.cpp
+++ Task/Task-1/main.cpp
@@ -1,28 +1,14 @@
+#ifndef HEADER_FORM
+#define HEADER_FORM
 #include <iostream>
-#include "Form.h"
-#include "Circle.h"
-#include "Point.h"
+#include <sstream>
 
-int main() 
+class Form
 {
-    unsigned int max_x = 1920;
-    unsigned int max_y = 1080;
+    public:
+        virtual ~Form() = 0;
+        virtual std::string ToString() const = 0;
+};
 
-    Circle::ValidateAndThrow(10);
+#endif
 
-    Circle c1(5, Point(10, 10));
-    c1.Draw();
-
-    Circle c2;
-    c2.Read(std::cin);
-    if (c2.radius > max_x || c2.radius > max_y) 
-    {
-        std::cout << "Invalid circle: radius too large" << std::endl;
-    }
-    else 
-    {
-        c2.Draw();
-    }
-
-    return 0;
-}
