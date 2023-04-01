445d0b49b1b7830cc25507bebe9c7ddb93e6a3bd
darkis <113136403+Bodichane@users.noreply.github.com>
2023-04-01 23:53:39 +0300

    Update Circle.cpp

--- Task/Task-1/Circle.cpp
+++ Task/Task-1/Circle.cpp
@@ -2,30 +2,21 @@
 
 Circle::Circle() : radius(0) {}
 
-Circle::Circle(unsigned int radius, const Point& center) : radius(radius), center(center) {}
+Circle::Circle(unsigned int radius, const Point& center) : radius(radius), center(center) 
+{
+    if (radius > 0)
+    {
+        radius = radius;
+    }
+    else
+    {
+        throw std::invalid_argument("Invalid circle radius.");
+    }
+}
 
 std::string Circle::ToString() const
 {
     std::stringstream ss;
     ss << "Circle: radius=" << radius << " center=" << center;
     return ss.str();
-}
-
-void Circle::FromString(const std::string& str)
-{
-    std::stringstream ss(str);
-    std::string dummy;
-    ss >> dummy >> dummy >> radius >> dummy >> center;
-}
-
-bool Circle::Validate(unsigned int radius) 
-{
-    return radius > 0;
-}
-
-void Circle::ValidateAndThrow(unsigned int radius) 
-{
-    if (!Validate(radius)) {
-        throw std::invalid_argument("Invalid circle radius");
-    }
 }
