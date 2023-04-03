7605f5bbbf64d9f89235d34cbbfad38114ce4693
darkis <113136403+Bodichane@users.noreply.github.com>
2023-04-03 20:32:20 +0300

    Update Form.h

--- Task/Task-1/Form.h
+++ Task/Task-1/Form.h
@@ -6,7 +6,14 @@
 class Form
 {
     public:
+        /**
+        * Default destructor.
+        */
         virtual ~Form() = 0;
+        /**
+        * Returns a string representation of the form.
+        * @return The string representation of the form.
+        */
         virtual std::string ToString() const = 0;
 };
 
