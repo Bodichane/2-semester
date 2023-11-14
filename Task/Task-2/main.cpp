e76e9ba9499cde7d224b7a8337f95c543521aa92
darkis <113136403+Bodichane@users.noreply.github.com>
2023-11-14 22:10:41 +0300

    Update main.cpp

--- Task/Task-2/main.cpp
+++ Task/Task-2/main.cpp
@@ -1,27 +1,23 @@
 #include "doubleLinkedList.h"
 #include <iostream>
+#include <string>
 
 int main() {
-    DoubleLinkedList<int> lst = { 1, 2, 3, 4 };
-    std::cout << "Initial list: " << lst.to_string() << std::endl;
+    std::string a;
+    std::string b;
+    std::string c;
 
-    lst.push_front(0);
-    lst.push_back(5);
-    std::cout << "List after push_front(0) and push_back(5): " << lst.to_string() << std::endl;
+    a = "1 test";
+    b = "2 test";
+    c = "3 test";
 
-    lst.remove(2);
-    std::cout << "List after removing element at index 2: " << lst.to_string() << std::endl;
+    DoubleLinkedList<std::string> ls;
+    ls.addNode(a);
+    ls.addNode(b);
+    ls.addNode(c);
 
-    lst[1] = 10;
-    std::cout << "List after setting element at index 1 to 10: " << lst.to_string() << std::endl;
+    return 0;
 
-    DoubleLinkedList<int> lst2 = lst;
-    std::cout << "Copy of the list: " << lst2.to_string() << std::endl;
-
-    lst2.clear();
-    std::cout << "Empty list: " << lst2.to_string() << std::endl;
-
-    std::cout << "Is the list empty? " << (lst.empty() ? "yes" : "no") << std::endl;
 
     return 0;
 }
