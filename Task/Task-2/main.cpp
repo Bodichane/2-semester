668f7b87d7f5efe51aa235b38850d208a014fad4
darkis <113136403+Bodichane@users.noreply.github.com>
2023-11-29 10:16:29 +0300

    Update main.cpp

--- Task/Task-2/main.cpp
+++ Task/Task-2/main.cpp
@@ -1,23 +1,9 @@
 #include "doubleLinkedList.h"
 #include <iostream>
-#include <string>
+
 
 int main() {
-    std::string a;
-    std::string b;
-    std::string c;
-
-    a = "1 test";
-    b = "2 test";
-    c = "3 test";
-
-    DoubleLinkedList<std::string> ls;
-    ls.addNode(a);
-    ls.addNode(b);
-    ls.addNode(c);
-
-    return 0;
-
-
-    return 0;
+  DoubleLinkedList<int> list = {1, 2, 3, 4, 5};
+  list.to_string();
+  return 0;
 }
