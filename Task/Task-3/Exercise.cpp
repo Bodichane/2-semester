a1022bea153ba60a662f192833c6297922554be9
darkis <113136403+Bodichane@users.noreply.github.com>
2023-12-21 00:48:30 +0300

    Update Exercise.cpp

--- Task/Task-3/Exercise.cpp
+++ Task/Task-3/Exercise.cpp
@@ -3,11 +3,7 @@
 #include <sstream>
 
 namespace miit::algebra {
-    Exercise::Exercise(Generator* generator, int size) : generator(generator), matrix(size, size) {
-        if (generator) {
-            matrix << *generator;
-        }
-    }
+    Exercise::Exercise(Matrix<int>, Generator* genarator) : matrix(matrix), generator(genarator) {}
 
     Exercise::~Exercise() {
         delete generator; 
