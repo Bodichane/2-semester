df6ffae7b80c04f8120003b5214fb6784a215d8d
darkis <113136403+Bodichane@users.noreply.github.com>
2023-12-21 00:52:16 +0300

    Update TaskExercise.cpp

--- Task/Task-3/TaskExercise.cpp
+++ Task/Task-3/TaskExercise.cpp
@@ -1,10 +1,17 @@
-#include "Task1Exercise.h"
+#include "TaskExercise.h"
 
 namespace miit::algebra {
+    TaskExercise::TaskExercise(Matrix<int> matrix, Generator* generator) : Exercise(matrix, generator) {}
 
-    Task1Exercise::Task1Exercise(Generator* generator, int size) : Exercise(generator, size) {}
-
-    void Task1Exercise::Task1() {
-        
+    void TaskExercise::Task1() {
+        for (size_t i = 0; i < matrix.getRows(); ++i) {
+            for (size_t j = 0; j < matrix.getColumns(); ++j) {
+                matrix[i][j] = generator.generate();
+            }
+        }
+    }
+    void TaskExercise::Task2() {
+        std::cout << "Task 2 Result: Matrix elements in a single line:\n";
+    std::cout << matrix.toFlatString() << "\n";
     }
 } 
