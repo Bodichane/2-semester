ed357fc52a7aa3ef508dc5c8d0df2eb5580c9a49
darkis <113136403+Bodichane@users.noreply.github.com>
2023-12-21 00:47:30 +0300

    Update Matrix.cpp

--- Task/Task-3/Matrix.cpp
+++ Task/Task-3/Matrix.cpp
@@ -33,6 +33,27 @@ namespace miit::algebra {
         return result.str();
     }
 
+    template<typename T>
+    size_t Matrix<T>::getRows() const {
+        return rows;
+    }
+
+    template<typename T>
+    size_t Matrix<T>::getColumns() const {
+        return cols;
+    }
+
+    template<typename T>
+    std::string Matrix<T>::toFlatString() const {
+        std::string result;
+        for (size_t i = 0; i < rows; ++i) {
+            for (size_t j = 0; j < columns; ++j) {
+                result += std::to_string(matrix[i][j]) + " ";
+            }
+        }
+        return result;
+    }
+
     template <typename T>
     Matrix<T>& Matrix<T>::operator=(const Matrix<T>& other) {
         if (this != &other) {
