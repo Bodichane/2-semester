da61402737ecf1c513f9f3db29c45890f980eb4d
darkis <113136403+Bodichane@users.noreply.github.com>
2023-12-24 22:27:02 +0300

    Update Generator.h

--- Task/Task-3/Generator.h
+++ Task/Task-3/Generator.h
@@ -1,20 +1,20 @@
 #pragma once
 
 namespace miit::algebra {
-    /*
-    * \brief Abstract base class representing a generator for matrix elements.
+    /**
+    * @brief Abstract base class representing a generator for matrix elements.
     */
     class Generator
     {
     public:
-        /*
-        * \brief Virtual destructor for Generator (pure virtual).
+        /**
+        * @brief Virtual destructor for Generator (pure virtual).
         */
         virtual ~Generator() = 0 {};
 
         /**
-        * \brief Pure virtual function to generate an integer.
-        * \return The generated integer.
+        * @brief Pure virtual function to generate an integer.
+        * @return The generated integer.
         */
         virtual int generate() = 0;
     };
