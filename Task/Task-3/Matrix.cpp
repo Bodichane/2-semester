9b7e24d77fd8e35f0e88895e95d127bbdf71e46c
darkis <113136403+Bodichane@users.noreply.github.com>
2023-12-24 22:29:17 +0300

    Update Matrix.cpp

--- Task/Task-3/Matrix.cpp
+++ Task/Task-3/Matrix.cpp
@@ -6,20 +6,7 @@ namespace miit::algebra {
     Matrix<T>::Matrix() {}
 
     template <typename T>
-    Matrix<T>::Matrix(const Matrix<T>& other) : data(other.data) {
-
-    }
-
-    template <typename T>
-    Matrix<T>::Matrix(Matrix<T>&& other) noexcept : data(std::move(other.data)) {
-     
-    }
-
-    template <typename T>
     Matrix<T>::Matrix(size_t rows, size_t cols) : data(rows, std::vector<T>(cols, T())) {}
-
-    template <typename T>
-    Matrix<T>::~Matrix() {}
 
     template <typename T>
     std::string Matrix<T>::toString() const {
@@ -41,14 +28,6 @@ namespace miit::algebra {
     template<typename T>
     size_t Matrix<T>::getColumns() const {
         return cols;
-    }
-
-    template <typename T>
-    Matrix<T>& Matrix<T>::operator=(const Matrix<T>& other) {
-        if (this != &other) {
-            data = other.data;
-        }
-        return *this;
     }
 
     template <typename T>
