db90c442816baaf1d7c8f1b2a5f44ded153c5799
darkis <113136403+Bodichane@users.noreply.github.com>
2023-12-24 22:26:22 +0300

    Update Exercise.h

--- Task/Task-3/Exercise.h
+++ Task/Task-3/Exercise.h
@@ -5,8 +5,8 @@
 #include <string>
 
 namespace miit::algebra {
-    /*
-    * \brief Represents an exercise involving a matrix and a generator.
+    /**
+    * @brief Represents an exercise involving a matrix and a generator.
     */
     class Exercise {
     protected:
@@ -14,25 +14,24 @@ namespace miit::algebra {
         Generator* generator;   
 
     public:
-        /*
-        * \brief Constructor for Exercise.
-        * \param matrix Represents the matrix.
-        * \param generator Pointer to a Generator for initializing the matrix.
+        /**
+        * @brief Constructor for Exercise.
+        * @param matrix Represents the matrix.
+        * @param generator Pointer to a Generator for initializing the matrix.
         */
-        Exercise(Matrix<int> matrix, Generator* generator);
+        Exercise(Matrix<int> matrix, Generator* genarator);
 
         /**
-        * \brief Destructor for Exercise.
+        * @brief Destructor for Exercise.
         */
         virtual ~Exercise();
 
-        /*
-        * \brief Pure virtual function representing the first task of the exercise.
+        /**
+        * @brief Pure virtual function representing the first task of the exercise.
         */
         virtual void Task1() = 0;
-
-        /*
-        * \brief Pure virtual function representing the second task of the exercise.
+        /**
+        * @brief Pure virtual function representing the second task of the exercise.
         */
         virtual void Task2() = 0;
     };
