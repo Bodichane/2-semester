b6fabb2bd73665cee4560cfddb31d7e0fc4c7c02
darkis <113136403+Bodichane@users.noreply.github.com>
2024-01-09 00:09:22 +0300

    Update doubleLinkedList.h

--- Task/Task-2/doubleLinkedList.h
+++ Task/Task-2/doubleLinkedList.h
@@ -1,23 +1,13 @@
 #pragma once
 
 #include <iostream>
+#include "Node.h"
 #include <initializer_list>
-#include <sstream>
-#include <stdexcept>
-#include <memory>
 
-template <typename T> class DoubleLinkedList {
-private:
-    struct Node {
-        T data;
-        std::shared_ptr<Node> next;
-        std::weak_ptr<Node> prev;
-        Node(const T& data) : data(data), next(nullptr), prev(nullptr) {}
-    };
-
-    Node* head;
-    Node* tail;
-    size_t size;
+class DoubleLinkedList {
+    std::shared_ptr<Node> head;
+    std::shared_ptr<Node> tail;
+    std::size_t size;
 
 public:
     /**
@@ -28,7 +18,7 @@ public:
     * @brief Constructs a new linked list initialized with the elements of the provided initializer list.
     * @param lst The initializer list of elements to initialize the linked list with.
     */
-    DoubleLinkedList(std::initializer_list<T> lst);
+    DoubleLinkedList(std::initializer_list<int> lst);
     /**
     * @brief Constructs a new linked list with the same elements as another linked list.
     * @param other The linked list to copy the elements from.
@@ -62,37 +52,38 @@ public:
     * @brief Adds a new node with the given data to the front of the linked list.
     * @param data The data to store in the new node.
     */
-    void push_front(const T& data);
+    void push_front(int value);
     /**
     * @brief Adds a new node with the given data to the back of the linked list.
     * @param data The data to store in the new node.
     */
-    void push_back(const T& data);
+    void push_back(int value);
+    /*
+    @brief a function which delete a Node in front of List.
+    */
+    void pop_front();
+    /*
+    @brief a function which delete a Node in back of List.
+    */
+    void pop_back();
     /**
     * @brief Returns a reference to the data stored in the node at the given index.
     * @param index The index of the node to retrieve the data from.
     * @return T& A reference to the data stored in the node at the given index.
     * @throws std::out_of_range If the index is out of bounds.
     */
-    T& operator[](size_t index);
+    int& operator[](size_t index);
     /**
     * @brief Returns a string representation of the linked list, with the elements separated by commas and enclosed in square brackets.
     * @return std::string The string representation of the linked list.
     */
-    std::string to_string() const;
+    void print();
     /**
      * @brief Removes the node at the given index from the linked list.
      * @param index The index of the node to remove.
      * @throws std::out_of_range If the index is out of bounds.
      */
     void remove(size_t index);
-    /**
-    * @brief Prints the string representation of the linked list to the given output stream.
-    * @param os The output stream to print the string representation to.
-    * @param lst The linked list to print the string representation of.
-    * @return std::ostream& The output stream after printing the string representation.
-    */
-    friend std::ostream& operator<<(std::ostream& os, const DoubleLinkedList<T>& lst);
     /**
     * @brief Assignment Operator= for DoubleLinkedList.
     * @param other The linked list to copy the elements from.
